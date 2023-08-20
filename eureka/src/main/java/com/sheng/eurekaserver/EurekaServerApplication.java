87fe6987699dd7facc9fdc1d371438de5d92e49b
shengjiangtao <shengjiangtao@zy1111.com>
2023-08-20 16:33:01 +0800

    init eureka

--- eureka/src/main/java/com/sheng/eurekaserver/EurekaServerApplication.java
+++ eureka/src/main/java/com/sheng/eurekaserver/EurekaServerApplication.java
@@ -1,12 +1,10 @@
 package com.sheng.eurekaserver;
 
-import org.bouncycastle.util.IPAddress;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
