2d17dda56926f9ac8bdfac5dbd630fc9e63b1109
shengjiangtao <shengjiangtao@zy1111.com>
2023-08-20 16:26:23 +0800

    init eureka

--- eureka/src/main/java/com/sheng/eurekaserver/EurekaServerApplication.java
+++ eureka/src/main/java/com/sheng/eurekaserver/EurekaServerApplication.java
@@ -1,8 +1,14 @@
 package com.sheng.eurekaserver;
 
+import org.bouncycastle.util.IPAddress;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +18,15 @@ public class EurekaServerApplication {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
+    @GetMapping("/ip")
+    @ResponseBody
+    public String ip() {
+        String ip = "localhost";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            //
+        }
+        return ip;
+    }
 }
