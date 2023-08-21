3f3f684cee7352ff2dbb9b963031d2cf17044528
shengjiangtao <shengjiangtao@zy1111.com>
2023-08-21 18:33:15 +0800

    init user ms

--- user/src/main/java/com/sheng/user/UserApplication.java
+++ user/src/main/java/com/sheng/user/UserApplication.java
@@ -3,13 +3,28 @@ package com.sheng.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.net.InetAddress;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Controller
 public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    @GetMapping("/actuator/info")
+    @ResponseBody
+    public String health() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            return "localhost";
+        }
     }
 
 }
